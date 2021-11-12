af5cefd3079e6ed08f9cdd2ff257cc2aafc25bdd
student <mpalacz00@gmail.com>
2021-11-12 11:11:27 +0100

    Zad2 poprawki Zad3

--- src/Zad2/Main.java
+++ src/Zad2/Main.java
@@ -5,15 +5,15 @@ public class Main {
         int[] array = new int[10];
         int min = 100;
         int max = 0;
+
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * (100 - 1)) + 1;
+            System.out.print(array[i]+ " ");
             if (array[i] < min) min = array[i];
             if (array[i] > max) max = array[i];
         }
 
-        for (int i = 0; i < array.length; i++)
-            System.out.println(array[i]);
-
+        System.out.println();
         System.out.println("Min: " + min);
         System.out.println("Max: " + max);
     }
---
+++ src/Zad3/Main.java
@@ -0,0 +1,24 @@
+package Zad3;
+
+public class Main {
+    public static void main(String args[]){
+        int[][] matrix = new int[10][10];
+
+        int x = 0;
+        int suma = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    matrix[i][j] = x;
+                    suma += x;
+                    x++;
+                } else matrix[i][j] = 0;
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("\nSuma przekatnej: " + suma);
+    }
+}
