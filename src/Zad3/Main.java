ab1626db8a8255279d9603021717176148e16c13
student <mpalacz00@gmail.com>
2021-11-12 11:14:28 +0100

    Zad3 final

--- src/Zad3/Main.java
+++ src/Zad3/Main.java
@@ -19,6 +19,6 @@ public class Main {
             System.out.print("\n");
         }
 
-        System.out.println("\nSuma przekatnej: " + suma);
+        System.out.println("Suma przekatnej: " + suma);
     }
 }
